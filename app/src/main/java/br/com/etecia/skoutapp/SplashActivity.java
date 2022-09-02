3b3722b158ba0bb23a780756dbbfbfdaec9db714
lndsilva <lndsilva@hotmail.com>
2022-09-01 21:46:26 -0300

    splash

---
+++ app/src/main/java/br/com/etecia/skoutapp/SplashActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.skoutapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+    }
+}
