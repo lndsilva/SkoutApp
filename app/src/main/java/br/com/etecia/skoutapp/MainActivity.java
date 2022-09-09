535522d6457d584fb0ee83054512671cdcc2c988
lndsilva <lndsilva@hotmail.com>
2022-09-08 21:51:47 -0300

    autenticando

--- app/src/main/java/br/com/etecia/skoutapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/skoutapp/MainActivity.java
@@ -3,12 +3,62 @@ package br.com.etecia.skoutapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+    //Declarar as variaveis globais que irão representar os objetos xml
+    EditText edtUsuario, edtSenha;
+    Button btnEntrar, btnSair;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //apresentar ao java os componentes do xml
+        edtSenha = findViewById(R.id.edtSenha);
+        edtUsuario = findViewById(R.id.edtUsuario);
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnSair = findViewById(R.id.btnSair);
+
+        //criando a ação de botão sair
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //criando as variaveis
+                String usuario, senha;
+
+                //atribuindo valores as variaveis
+                usuario = edtUsuario.getText().toString();
+                senha = edtSenha.getText().toString();
+
+                //validando o usuario e senha
+                if (usuario.equals("etecia") && senha.equals("etecia")) {
+
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Usuário ou senha inválidos!!!",
+                            Toast.LENGTH_SHORT).show();
+                    edtUsuario.setText("");
+                    edtSenha.setText("");
+                    edtUsuario.requestFocus();
+                }
+
+            }
+        });
     }
+
+
 }
